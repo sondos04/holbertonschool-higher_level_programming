f97e51c3bf84b4265b7892b7ef386a7ee3f9e362
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2025-11-28 01:22:44 +0000

    print alphapt except e & q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in list(
-    range(97, 101)) + list(range(102, 113)) + list(range(114, 123)):
+                    range(97, 101)) + list(range(102, 113)) + list(range(114, 123)):
     print("{}".format(chr(letter)), end="")
