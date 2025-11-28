71b861b85a5e286a117c68870daed3029a3d3dc6
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2025-11-28 04:05:14 +0000

    math operations

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    return (a + b)
+def add(a, b): return (a + b)
 
+def sub(a, b): return (a - b)
 
-def sub(a, b):
-    return (a - b)
+def mul(a, b): return (a * b)
 
-
-def mul(a, b):
-    return (a * b)
-
-
-def div(a, b):
-    return int(a / b)
+def div(a, b): return int(a / b)
