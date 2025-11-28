7290a7a95c87ae7782de7adb67eaf9251c4b43d5
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2025-11-28 04:09:20 +0000

    math operations

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-def add(a, b): return (a + b)
 
-def sub(a, b): return (a - b)
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
-def mul(a, b): return (a * b)
-
-def div(a, b): return int(a / b)
