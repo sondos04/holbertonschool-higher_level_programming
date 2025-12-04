8d7dc7c756f15ac26a69ad3fbc0fa0190447279c
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-04 14:17:59 +0000

    fix name of the list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-       if i < 0 or i>= len(my_list):
+       if idx < 0 or idx >= len(my_list):
            return my_list
-       if i == idx:
-           my_list[i] = element
+       
+       my_list[idx] = element
            return my_list
