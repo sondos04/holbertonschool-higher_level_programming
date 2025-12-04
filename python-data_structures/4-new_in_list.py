5a8b1bd5db13beabbf33e8dab510d7f7b9f81b97
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-04 22:16:30 +0300

    change value of index

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
     for i in range(len(my_list)):
         if i < 0 or i >= len(my_list):
             return my_list
         if i == idx:
-            new_list = my_list.copy()
             new_list[i] = element
             return new_list
