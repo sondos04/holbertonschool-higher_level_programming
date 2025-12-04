d2c42133cd653199fe987d3b6bfdff6b9293b6d1
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-04 23:06:10 +0300

    print matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,3 +6,4 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(row[idx]), end=" ")
             else:
                 print("{:d}".format(row[idx]), end="")
+                print("\n", end="")
