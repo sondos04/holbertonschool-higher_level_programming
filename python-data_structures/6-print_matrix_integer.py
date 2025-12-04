c477425b5064c079400f53289a5426094f0d475e
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-04 23:02:34 +0300

    print matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for idx in range(len(row)):
+            if idx != len(row) - 1:
+                print("{:d}".format(row[idx]), end=" ")
+            else:
+                print("{:d}".format(row[idx]), end="")
