2f3a765a5e2382f5914c4cd1aab8cc56f5c6d90e
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-04 13:55:25 +0000

    edit spaces

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-            if i < 0 or 
-                    i >= len(my_list):
+            if i < 0 
+                    or  i >= len(my_list):
                 return None
             if i == idx:
                 return my_list[i]
