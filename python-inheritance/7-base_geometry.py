87d37eb865ce2aba808e0c22aea7d9fc2312cd62
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-20 01:31:18 +0000

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module to check if an object is exactly an instance of a specified class
+"""
+
+
+class BaseGeometry:
+    """
+    A class that defines a base geometry.
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message 'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that value is an integer greater than 0.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
