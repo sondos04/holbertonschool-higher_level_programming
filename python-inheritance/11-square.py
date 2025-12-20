d7f8574b02c23e514f7dc8a1fe2dd3320a1ec904
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-20 01:35:27 +0000

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module to check if an object is exactly an instance of a specified class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class that defines a square, inheriting from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a given size.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
