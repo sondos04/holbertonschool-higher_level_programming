e967eb3c5197fef5dd92aec555d8761b33cba574
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-20 01:29:01 +0000

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module to check if an object is exactly an instance of a specified class
+"""
+
+
+class BaseGeometry:
+    """
+    A class that defines a base geometry.
+    """
+    pass
