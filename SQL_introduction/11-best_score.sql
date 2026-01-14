9212d5012afcf1a533c7d4db0e75dd8f1734c98b
sondos04 <sondos.alrubaih04@gmail.com>
2026-01-14 20:25:29 +0000

    lists all records with a score >= 10 in the table second_table

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- list all records with score >= 10 in second_table
 
-SELECT score, name FROM second_table ORDER BY score DESC  WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
