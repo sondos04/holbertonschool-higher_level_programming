229a89ddf5a45d586702ac85171eeddaf9eea29c
sondos04 <sondos.alrubaih04@gmail.com>
2026-01-14 20:07:20 +0000

    create table and fill it

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- creat table in database hbtn_0c_0 and add values
+
+CREAT TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10),(2, "Alex",3),(3, "Bob", 14), (4, "George", 8);
